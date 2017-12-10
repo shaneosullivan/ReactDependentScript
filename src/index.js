5624c840d1754a3d0390c195c76051f65bb332e5
Shane O'Sullivan <shaneosullivan1@gmail.com>
2017-12-09 20:23:53 -0800

    Do not query the document every time the component is mounted, it is unnecessary

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+// Globally track the nodes previously inserted so each is only inserted once
+let scriptUrls = {};
+let sheetUrls = {};
+
 export default class ReactDependentScript extends Component {
   constructor() {
     super();
@@ -10,40 +14,40 @@ export default class ReactDependentScript extends Component {
     const scripts = this.props.scripts;
     const stylesheets = this.props.stylesheets;
 
-    // Load the stylesheets first
+    // Load the stylesheets first, but don't wait for them to complete, as
+    // nothing will break.
     if (stylesheets && stylesheets.length > 0) {
-      const unloadedSheets = stylesheets.filter(sheet => {
-        return !document.body.querySelector(`link[data-href='${sheet}']`);
-      });
-
-      unloadedSheets.forEach(sheet => {
-        const sheetNode = document.createElement('link');
-        sheetNode.setAttribute('rel', 'stylesheet');
-        sheetNode.setAttribute('href', sheet);
-        sheetNode.setAttribute('data-href', sheet);
-        document.body.appendChild(sheetNode);
+      stylesheets.forEach(sheet => {
+        if (!sheetUrls[sheet]) {
+          const sheetNode = document.createElement('link');
+          sheetNode.setAttribute('rel', 'stylesheet');
+          sheetNode.setAttribute('href', sheet);
+          document.body.appendChild(sheetNode);
+        }
+        sheetUrls[sheet] = 1;
       });
     }
 
     // Look for the script in the body. If not there, inject it.
     if (scripts && scripts.length > 0) {
       const unloadedScripts = scripts.filter(script => {
-        return !document.body.querySelector(`script[data-src='${script}']`);
+        return !scriptUrls[script];
       });
 
       this.setState({ loadingCount: unloadedScripts.length }, () => {
         unloadedScripts.forEach(script => {
+          // if (!scriptUrls[script])
+          scriptUrls[script] = 1;
           const scriptNode = document.createElement('script');
           scriptNode.type = 'text/javascript';
           scriptNode.src = script;
-          scriptNode.setAttribute('data-src', script);
           scriptNode.addEventListener('load', this._handleLoad);
           document.body.appendChild(scriptNode);
         });
       });
     }
   }
-  //
+
   render() {
     if (this.state.loadingCount === 0) {
       if (this.props.renderChildren) {
@@ -56,7 +60,6 @@ export default class ReactDependentScript extends Component {
   }
 
   _handleLoad = () => {
-    console.log('_handleLoad', this.state.loadingCount, 'remaining');
     this.setState({ loadingCount: this.state.loadingCount - 1 });
   };
 }
