5dc2745a4d5a0429a41f6fc3a43655ccb7238f17
Shane O'Sullivan <shane@joinpromise.com>
2019-06-07 15:45:57 -0700

    Rebuild the source

--- es/index.js
+++ es/index.js
@@ -4,7 +4,15 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+// Globally track the nodes previously inserted so each is only inserted once
+var scriptUrls = {};
+var sheetUrls = {};
+
+function isBrowser() {
+  return typeof document !== "undefined" && typeof window !== "undefined";
+}
 
 var ReactDependentScript = function (_Component) {
   _inherits(ReactDependentScript, _Component);
@@ -15,7 +23,6 @@ var ReactDependentScript = function (_Component) {
     var _this = _possibleConstructorReturn(this, _Component.call(this));
 
     _this._handleLoad = function () {
-      console.log('_handleLoad', _this.state.loadingCount, 'remaining');
       _this.setState({ loadingCount: _this.state.loadingCount - 1 });
     };
 
@@ -26,53 +33,69 @@ var ReactDependentScript = function (_Component) {
   ReactDependentScript.prototype.componentWillMount = function componentWillMount() {
     var _this2 = this;
 
-    var scripts = this.props.scripts;
-    var stylesheets = this.props.stylesheets;
+    if (!isBrowser()) {
+      // When mounted during server side rendering, do not
+      // attempt to mutate the document, as it will break.
+      return;
+    }
+    var _props = this.props,
+        scripts = _props.scripts,
+        stylesheets = _props.stylesheets;
 
-    // Load the stylesheets first
+    // Load the stylesheets first, but don't wait for them to complete, as
+    // nothing will break.
+
     if (stylesheets && stylesheets.length > 0) {
-      var unloadedSheets = stylesheets.filter(function (sheet) {
-        return !document.body.querySelector('link[data-href=\'' + sheet + '\']');
-      });
-
-      unloadedSheets.forEach(function (sheet) {
-        var sheetNode = document.createElement('link');
-        sheetNode.setAttribute('rel', 'stylesheet');
-        sheetNode.setAttribute('href', sheet);
-        sheetNode.setAttribute('data-href', sheet);
-        document.body.appendChild(sheetNode);
+      stylesheets.forEach(function (sheet) {
+        if (!sheetUrls[sheet]) {
+          var sheetNode = document.createElement("link");
+          sheetNode.setAttribute("rel", "stylesheet");
+          sheetNode.setAttribute("href", sheet);
+          document.body.appendChild(sheetNode);
+        }
+        sheetUrls[sheet] = 1;
       });
     }
 
     // Look for the script in the body. If not there, inject it.
     if (scripts && scripts.length > 0) {
       var unloadedScripts = scripts.filter(function (script) {
-        return !document.body.querySelector('script[data-src=\'' + script + '\']');
+        return !scriptUrls[script.source || script];
       });
 
       this.setState({ loadingCount: unloadedScripts.length }, function () {
         unloadedScripts.forEach(function (script) {
-          var scriptNode = document.createElement('script');
-          scriptNode.type = 'text/javascript';
-          scriptNode.src = script;
-          scriptNode.setAttribute('data-src', script);
-          scriptNode.addEventListener('load', _this2._handleLoad);
+          var src = script.source || script;
+          scriptUrls[src] = 1;
+          var scriptNode = document.createElement("script");
+          scriptNode.type = "text/javascript";
+          scriptNode.src = src;
+          scriptNode.addEventListener("load", _this2._handleLoad);
+          scriptNode.addEventListener("error", (script.onerror, // error callback
+          script.not_required && _this2._handleLoad
+          // using not_required,
+          // user can choose to render children,
+          // even on error
+          ));
           document.body.appendChild(scriptNode);
         });
       });
     }
   };
-  //
-
 
   ReactDependentScript.prototype.render = function render() {
-    if (this.state.loadingCount === 0) {
-      if (this.props.renderChildren) {
-        return this.props.renderChildren();
+    var _props2 = this.props,
+        renderChildren = _props2.renderChildren,
+        children = _props2.children,
+        loadingComponent = _props2.loadingComponent;
+
+    if (isBrowser() && this.state.loadingCount === 0) {
+      if (renderChildren) {
+        return renderChildren();
       }
-      return this.props.children;
+      return children;
     } else {
-      return this.props.loadingComponent || null;
+      return loadingComponent || null;
     }
   };
 
